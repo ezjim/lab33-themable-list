c1fa77bdfe88827980bc94b21810adb78a9ef599
james eserjose <james.re@yahoo.com>
2020-05-08 08:47:24 -0700

    updated toggles need css

--- src/hooks/CharacterProvider.jsx
+++ src/hooks/CharacterProvider.jsx
@@ -1,13 +1,35 @@
-import { createContext } from "react";
+import React, { createContext, useState, useContext } from 'react';
 import PropTypes from 'prop-types';
-import { fetchCharacters } from "../services/fetch";
 
-const CharacterContext = createContext()
 
-const getData = {
-  characters: fetchCharacters
+const ToggleSwitch = createContext();
+
+export const ToggleSwitchkProvider = ({ children }) => {
+  const [toggleOn, setToggleOn] = useState('lights on');
+
+  const toggle = ({ target }) => {
+    if(target.checked) setToggleOn('lights on');
+    if(!target.checked) setToggleOn('lights off');
+  };
+
+  return (
+    <ToggleSwitch.Provider value={{ toggleOn, toggle }}>
+      {children}
+    </ToggleSwitch.Provider>
+  );
+};
+//specify that anything renderable 
+ToggleSwitchkProvider.propTypes = {
+  children: PropTypes.node
+};
+//useContext hook allows passing data to children elements without using redux
+
+export const useToggle = () => {
+  const { toggle } = useContext(ToggleSwitch);
+  return toggle;
 };
 
-export const CharacterProvider = ({ children }) => {
-  const [chars, setChars] = useState('characters');
-}
+export const useToggleOn = () => {
+  const { toggleOn } = useContext(ToggleSwitch);
+  return toggleOn;
+};
